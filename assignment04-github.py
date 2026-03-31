67a10137a5f42c800bb7279d4121d2b2f21a8a64
Marcella Morgan <conactmarcellamorgan@gmail.com>
2026-03-31 22:45:16 +0100

    Working on assignment04

--- assignment04-github.py
+++ assignment04-github.py
@@ -10,7 +10,7 @@ content = content.replace("is", "is not")
 
 with open("file.txt", "w") as file:
     file.write(content)
-'''
+
 # Well that didn't work - why didn't just replace is with is not? it reads This not is not a test. try a 
 #different word
 
@@ -18,3 +18,14 @@ content = content.replace("That", "This")
 
 with open("file.txt", "w") as file:
     file.write(content)
+'''
+
+# change content to content 2 and words in lowercase
+
+content2 = content.replace("this", "that")
+
+print (content2)
+
+with open("file.txt", "w") as file:
+    file.write(content2)
+
